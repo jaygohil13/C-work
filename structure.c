3d80694f78b9acbee4b1be7b2844f803cb04927f
jaygohil13 <109522888+jaygohil13@users.noreply.github.com>
2022-08-04 09:44:37 +0530

    jay

--- structure.c
+++ structure.c
@@ -1,22 +1,25 @@
 #include<stdio.h>
     
 struct employee{
-	int eid;
+	int i, eid;
 	char name[10];
 	float esal;
 };
 
 void main()
+
 {
 	struct employee e1;
 	
-	printf("Enter Id :");
+	printf("\n=======ACCEPT EMPLOYEE 1 DETAILS=======");
+	printf("\n\nEnter Id :");
 	scanf("%d",&e1.eid);
 	printf("\nEnter name :");
 	scanf("%s",&e1.name  );
 	printf("\nEnter salary :");
 	scanf("%f",&e1.esal);
 	
+	printf("\n\n=======DISPLAY EMPLOYEE 1 DETAILS=======");
 	printf("\n\nId = %d",e1.eid);
 	printf("\n\nname = %s",e1.name);
 	printf("\n\nsalary = %.2f",e1.esal);
